98d9a319abce1dd93edcd8083d8fbfc6f985b14b
mehedi20695 <mehedi.m000@gmail.com>
2025-01-17 03:00:15 +0600

    Add biodata of specific id api with verify token

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzi65.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,7 +34,7 @@ async function run() {
         // jwt related api
         app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '6h' });
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' });
             res.send({ token });
         })
 
@@ -44,7 +44,6 @@ async function run() {
                 return res.status(401).send({ message: 'unauthorized access' });
             }
             const token = req.headers.authorization.split(' ')[1];
-            console.log(token);
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
                 if (err) {
                     return res.status(401).send({ message: 'unauthorized access' })
@@ -76,6 +75,13 @@ async function run() {
             const result = await biodataCollection.find().toArray();
             res.send(result)
         })
+
+        app.get('/biodata/:id', verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const biodata = await biodataCollection.findOne(query);
+            res.send(biodata);
+        });
         // todo: post request for biodata for create biodata
 
         // stories related api
